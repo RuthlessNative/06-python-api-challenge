202cb2f8c9595803390c1b1b9bfd01920dc9a4a0
RuthlessNative <daniel.g.ramirez13@gmail.com>
2021-02-07 23:50:19 -0500

    Attempt to create call for data

---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "ce0bdd9e49aeb69c50c558a23e4e08e2"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
--- api_keys-Copy1.py
+++ api_keys-Copy1.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ce0bdd9e49aeb69c50c558a23e4e08e2"
 
 # Google API Key
 g_key = "YOUR KEY HERE!"
